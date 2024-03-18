bcc09c26e34e92d2d13d00843baf0dbd24d8fb8a
Chayank Das <67795717+Chayank-Das@users.noreply.github.com>
2024-03-18 10:58:31 +0530

    BAJAJapi.js

--- api.js
+++ api.js
@@ -1,29 +1,38 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
-app.use(express.json());
 
-app.post('/api/array', (req, res) => {
-  const inputArray = req.body.array;
-  const userId = req.body.userId;
-  const emailId = req.body.emailId;
-  const collegeRollNumber = req.body.collegeRollNumber;
+app.use(bodyParser.json());
 
-  const status = 'success';
+app.post("/array", (req, res) => {
+  const inputArray = req.body.inputArray;
+
+  const status = "success";
+  const userId = inputArray[0];
+
+  const emailId = inputArray[1];
+
+  const collegeRollNumber = inputArray[2];
+
   const evenNumbers = inputArray.filter(num => num % 2 === 0);
-  const oddNumbers = inputArray.filter(num => num % 2 !== 0);
-  const alphabets = inputArray.filter(item => typeof item === 'string').map(item => item.toUpperCase());
 
-  res.json({
+  const oddNumbers = inputArray.filter(num => num % 2 !== 0);
+
+  const alphabets = inputArray.filter(char => char.match(/[a-z]/i));
+  const uppercaseAlphabets = alphabets.map(char => char.toUpperCase());
+
+  res.send({
     status,
     userId,
     emailId,
     collegeRollNumber,
     evenNumbers,
     oddNumbers,
-    alphabets
+    uppercaseAlphabets
   });
 });
 
 app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+  console.log(`Server is running on port 5000.`);
 });
